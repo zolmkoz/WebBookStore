512b85aec1a7f06cbad560c0f994c3d40023f7f5
Papatronic <zolmkoz1@gmail.com>
2022-02-15 11:54:29 +0700

    Create Topic & Publishing List cate

--- WebBookStore/Controllers/BookStoreController.cs
+++ WebBookStore/Controllers/BookStoreController.cs
@@ -3,15 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebBookStore.Models;
 
 namespace WebBookStore.Controllers
 {
     public class BookStoreController : Controller
     {
+        //Tạo 1 đối tượng để quản lý CSDL
+        dbQLBookstoreDataContext data = new dbQLBookstoreDataContext();
+        private List<Sach> Laysachmoi(int count)
+        {
+            //Sắp xếp
+            return data.Saches.OrderByDescending(a => a.NgayCapNhat).Take(count).ToList();
+        }
         // GET: BookStore
         public ActionResult Index()
         {
-            return View();
+            var sachmoi = Laysachmoi(5);
+            return View(sachmoi);
+        }
+        public ActionResult Topic()
+        {
+            var chude = from cd in data.ChuDes select cd;
+            return PartialView(chude);
+        }
+        public ActionResult Publishingcompany()
+        {
+            var nhaxuatban = from cd in data.NhaXuatBans select cd;
+            return PartialView(nhaxuatban);
         }
     }
 }
