755668ebdfb89133cdb614e857f995826a252230
Papatronic <zolmkoz1@gmail.com>
2022-02-15 10:46:41 +0700

    Add project files.

---
+++ WebBookStore/Controllers/BookStoreController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebBookStore.Controllers
+{
+    public class BookStoreController : Controller
+    {
+        // GET: BookStore
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebBookStore/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebBookStore.MvcApplication" Language="C#" %>
