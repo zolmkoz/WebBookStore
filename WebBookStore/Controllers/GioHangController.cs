3afdbd95d68600e4a63ae2c6538e5dc8d39513bc
Papatronic <zolmkoz1@gmail.com>
2022-02-17 13:32:24 +0700

    Edit something CSS, Build funcition Clear, Update qty in Cart

--- WebBookStore/Controllers/GioHangController.cs
+++ WebBookStore/Controllers/GioHangController.cs
@@ -11,11 +11,11 @@ namespace WebBookStore.Controllers
     {
         //Tạo 1 đối tượng để quản lý CSDL
         dbQLBookstoreDataContext data = new dbQLBookstoreDataContext();
-        // GET: GioHang
+        // GET: Giohang
         public List<Giohang> LayGioHang()
         {
             List<Giohang> lstGiohang = Session["Giohang"] as List<Giohang>;
-            //Nếu tồn tại, gán vào Card
+            //Nếu tồn tại, gán vào Cart
             if (lstGiohang == null)
             {
                 lstGiohang = new List<Giohang>();
@@ -27,7 +27,7 @@ namespace WebBookStore.Controllers
         {
             //Get Session
             List<Giohang> lstGiohang = LayGioHang();
-            //Checked books add to card
+            //Checked books add to cart
             Giohang SanPham = lstGiohang.Find(n => n.iMaSach == iMaSach);
             if (SanPham == null)
             {
@@ -64,7 +64,7 @@ namespace WebBookStore.Controllers
             }
             return iTongTien;
         }
-        //Create card
+        //Create Shopping cart
         public ActionResult GioHang()
         {
             List<Giohang> lstGiohang = LayGioHang();
@@ -84,7 +84,7 @@ namespace WebBookStore.Controllers
             return PartialView();
         }
 
-        //Delete Card
+        //Delete Shopping Cart
         public ActionResult XoaGiohang(int iMaSP)
         {
             //Get card from Session
@@ -102,6 +102,30 @@ namespace WebBookStore.Controllers
                 return RedirectToAction("Index", "BookStore");
             }
             return RedirectToAction("GioHang");
+        }
+
+        //Update Shopping Cart
+        public ActionResult CapnhatGiohang(int iMaSP, FormCollection f)
+        {
+            //Get card from Session
+            List<Giohang> lstGiohang = LayGioHang();
+            //Checked books in session["Giohang"]
+            Giohang SanPham = lstGiohang.SingleOrDefault(n => n.iMaSach == iMaSP);
+            //If already exists, edit the quantity
+            if (SanPham != null)
+            {
+                SanPham.iSoLuong = int.Parse(f["txtSoluong"].ToString());
+            }
+            return RedirectToAction("GioHang");
+        }
+
+        //Clear Shopping Cart
+        public ActionResult XoaTatcaGiohang()
+        {
+            //Get cart from Session
+            List<Giohang> lstGiohang = LayGioHang();
+            lstGiohang.Clear();
+            return RedirectToAction("Index", "BookStore");
         }
     }
 }
